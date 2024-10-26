16d0f0652b472b6588b40b34d48dff8556d478b7
asolovey83 <solovey.andriy@gmail.com>
2024-10-26 20:52:20 +0300

    Mongo DB Insert Operation

--- Lecture Notebooks/Mongo/mongotest.py
+++ Lecture Notebooks/Mongo/mongotest.py
@@ -19,10 +19,111 @@ try:
 except Exception as e:
     print(e)
 
-d = {'name': 'sudhanshu', 'emailid': 'sudhanshu@ineuron.ai', 'surname':'kumar'}
+data = {'name': 'sudhanshu', 
+    'emailid': 'sudhanshu@ineuron.ai', 
+    'surname':'kumar', 
+    'subject': ['data science', 'data analytisc', 'engineering']}
 
-db1 = client['mongotest']
-coll = db1['test']
-coll.insert_one(d)
+List_of_records = [
+    {
+        'company_name': 'iNeuron',
+        'product': 'Affordable AI',
+        'course' : 'Data Analytics'
+    },
 
-# Figure out how to push/pull from/to GitHub
+    {
+        'company_name': 'iNeuron',
+        'product': 'Affordable AI',
+        'course' : 'Big Data'
+    },
+
+    {
+        'company_name': 'iNeuron',
+        'product': 'Affordable AI',
+        'course' : 'Science'
+    },
+]
+
+data3 =[
+	{
+		"id": "0001",
+		"type": "donut",
+		"name": "Cake",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" },
+						{ "id": "1003", "type": "Blueberry" },
+						{ "id": "1004", "type": "Devil's Food" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5007", "type": "Powdered Sugar" },
+				{ "id": "5006", "type": "Chocolate with Sprinkles" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0002",
+		"type": "donut",
+		"name": "Raised",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0003",
+		"type": "donut",
+		"name": "Old Fashioned",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	}
+]
+
+database = client['myinfo']
+collection = database['sudh']
+#collection.insert_one(data)
+collection.insert_many(data3)
+
+
+result = collection.find()
+
+for i in result:
+    print(i)
+
+# 1:25 min
---
+++ Lecture Notebooks/Mongo/mongotest3.py
@@ -0,0 +1,97 @@
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://soloveyandriy:tAAHDE5wfixW07ew@cluster0.qee4t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+db = client.server_info
+
+print(db)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+
+
+data = [
+    {
+        "item": "canvas",
+        "qty": 100,
+        "size": {"h": 28, "w": 35.5, "uom": "cm"},
+        "status": "A"
+    },
+    {
+        "item": "journal",
+        "qty": 25,
+        "size": {"h": 14, "w": 21, "uom": "cm"},
+        "status": "B"
+    },
+    {
+        "item": "sketchpad",
+        "qty": 50,
+        "size": {"h": 20, "w": 25, "uom": "cm"},
+        "status": "A"
+    },
+    {
+        "item": "watercolor set",
+        "qty": 15,
+        "size": {"h": 5, "w": 30, "uom": "cm"},
+        "status": "C"
+    },
+    {
+        "item": "easel",
+        "qty": 10,
+        "size": {"h": 150, "w": 60, "uom": "cm"},
+        "status": "B"
+    },
+    {
+        "item": "paintbrush",
+        "qty": 200,
+        "size": {"h": 1, "w": 20, "uom": "cm"},
+        "status": "A"
+    },
+    {
+        "item": "acrylic paint",
+        "qty": 30,
+        "size": {"h": 10, "w": 10, "uom": "cm"},
+        "status": "C"
+    },
+    {
+        "item": "charcoal sticks",
+        "qty": 40,
+        "size": {"h": 2, "w": 15, "uom": "cm"},
+        "status": "B"
+    },
+    {
+        "item": "oil paint",
+        "qty": 20,
+        "size": {"h": 8, "w": 15, "uom": "cm"},
+        "status": "A"
+    },
+    {
+        "item": "portfolio",
+        "qty": 35,
+        "size": {"h": 40, "w": 30, "uom": "cm"},
+        "status": "B"
+    }
+]
+
+database = client['inventory']
+collection = database['table']
+
+#collection.insert_many(data)
+
+record = collection.find()
+
+for i in record:
+    print(i)
+
+# 2:00
